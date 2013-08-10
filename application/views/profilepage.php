8d5b2cc84488d6e8164ac10e5c5c3bdfce6eed75
nhikieuod2012@gmail.com <nhikieuod2012@gmail.com>
2013-08-10 10:55:17 +0700

    Signed-off-by: nhikieuod2012@gmail.com <nhikieuod2012@gmail.com>

--- application/views/profilepage.php
+++ application/views/profilepage.php
@@ -1,3 +1,21 @@
 <?php
-	var_dump($postresults);
+	$divsep = 1;
+	foreach($postresults as $perpost){
+		if($divsep == 1){
+			echo '<div id="1">';
+		}
+		if(($divsep > 2) && ($divsep%4==1)){
+			$divindex = intval($divsep/4)+1;
+			echo '<div id="'.$divindex.'">';
+		}
+		echo $perpost->babershopname.'--';
+		if(($divsep > 2) && ($divsep%4==0)){
+			echo '</div>';
+		}		
+		$divsep++;
+	};
+	$divsep -= 1;
+	if ($divsep%4 > 0){
+		echo '</div>';
+	}
 ?>
