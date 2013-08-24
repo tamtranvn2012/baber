4e5409f4603c0d4d68ee8bc17648bc72ab2230dc
nhanduyen <ntnhanduyen0806@gmail.com>
2013-08-24 14:18:19 +0700

    duyen

--- application/views/approvelistingall.php
+++ application/views/approvelistingall.php
@@ -1,18 +1,20 @@
 <div class="container">
-
 <?php
- echo '<div class="span7 offset2 well text-center" style="line-height:30px;">';
+ echo '<div class="row well">';
 foreach($allinfo as $perinfo){
-
-	echo 'Bpid:'.$perinfo['bpid'].' , Babershop name: '.$perinfo['babershopname'];
+    echo '<ul class="span3">';
+	echo '<li>'.'Bpid:'.$perinfo['bpid'];
+    echo '<li>'. 'Babershop name:'.$perinfo['babershopname'].'</li>';
 	
 	if($perinfo['isapproved'] == 1){
 		$statusapprove = 'Yes';
 	}else{
 		$statusapprove = 'Waiting';
 	}
-	echo ' | Approve status:'.$statusapprove;
+	echo '<li>'.'Approve status:'.$statusapprove.'</li>';
+    echo '</ul>';
 }
+    
 echo '</div>';
 ?>
 </div>
